237c5fa671b7e2a2bf9285523b03dfb748e35a25
Mirana-19 <martysik19@gmail.com>
2023-10-01 22:10:01 +0000

    Deploying to gh-pages from @ Mirana-19/goit-js-hw-9@7d507bdfb4e8e3ddf5697eca4a1e993e84decb17 🚀

---
+++ 01-color-switcher.c9a2ea9e.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.startBtn.addEventListener("click",(function(){n=setInterval((()=>{var t;t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,document.body.style.backgroundColor=t}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.disabled=!1,t.stopBtn.disabled=!0}));let n=null;t.stopBtn.disabled=!0;
+//# sourceMappingURL=01-color-switcher.c9a2ea9e.js.map
